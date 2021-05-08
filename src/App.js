58690c513cfe51287f66f97341eb5452f9b9630e
memo <majd.khoury93@gmail.com>
2021-05-07 20:50:52 -0400

    feat: 3d environment added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import React, { useEffect } from "react";
 import web3 from "./ethereum/web3";
 import messageCreator from "./ethereum/messageCreator";
 
+// aframe imports
+import "aframe";
+import {Entity} from "aframe-react";
+
 const App = () => {
   useEffect(() => {
     const getAccount = async () => {
@@ -18,7 +22,48 @@ const App = () => {
     getMessages();
     getAccount();
   }, []);
-  return <div>hello</div>;
+  return (
+    <a-scene>
+      <a-sky color="#333333"></a-sky>
+      <a-camera></a-camera>{" "}
+      <Entity
+        geometry={{ primitive: "plane", width: 10, height: 10 }}
+        position="0 0 -4"
+        rotation="-90 0 0"
+        material="color: blue"
+      ></Entity>
+      <Entity
+        geometry={{ primitive: "plane", width: 10, height: 5 }}
+        position="0 2.5 -9"
+        rotation="0 0 0"
+        material="color: yellow"
+      ></Entity>
+      <Entity
+        geometry={{ primitive: "plane", width: 10, height: 5 }}
+        position="5 2.5 -4"
+        rotation="0 -90 0"
+        material="color: green"
+      ></Entity>
+      <Entity
+        geometry={{ primitive: "plane", width: 10, height: 5 }}
+        position="-5 2.5 -4"
+        rotation="0 90 0"
+        material="color: purple"
+      ></Entity>
+      <Entity
+        geometry={{ primitive: "plane", width: 10, height: 5 }}
+        position="0 2.5 1"
+        rotation="0 -180 0"
+        material="color: red"
+      ></Entity>
+      <Entity
+        geometry={{ primitive: "plane", width: 10, height: 10 }}
+        position="0 5 -4"
+        rotation="90 0 0"
+        material="color: #7BC8A4"
+      ></Entity>
+    </a-scene>
+  );
 };
 
 export default App;
