bf91967fbd9874ebd5a6b9cc01e936351198202c
memo <majd.khoury93@gmail.com>
2021-05-09 21:25:08 -0400

    feat: button sends tx

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import messageCreator from "./ethereum/messageCreator";
 import "aframe";
 import { Entity } from "aframe-react";
 require("aframe-super-keyboard");
+require("aframe-ui-widgets");
 
 const App = () => {
   // messages state
@@ -53,13 +54,50 @@ const App = () => {
     );
   });
 
+  const sendMessage = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log(accounts[0]);
+    await messageCreator.methods.addPaper().send({
+      from: accounts[0],
+    });
+  };
+
   return (
     <a-scene>
+      <a-assets>
+        <a-mixin
+          id="beveled-square"
+          geometry="primitive: cone; radiusTop: 0.15; radiusBottom: 0.19; height: 0.02; segmentsRadial: 4; segmentsHeight: 1"
+          rotation="0 45 0"
+        ></a-mixin>
+        <a-mixin
+          id="square"
+          geometry="primitive: box; width: 0.18; height: 0.025; depth: 0.18;"
+          position="0 0.02 0"
+        ></a-mixin>
+        <a-mixin id="blue" material="color: #1E2768;"></a-mixin>
+        <a-mixin id="darkgreen" material="color: #22FF90;"></a-mixin>
+        <a-mixin id="yellow" material="color: #FFF88E;"></a-mixin>
+        <a-mixin id="offset" position="0 0.01 0"></a-mixin>
+      </a-assets>
+      {/* sky and camera */}
       <a-sky color="#333333"></a-sky>
-      <a-camera></a-camera>
+      <a-camera>
+        {/* Mouse cursor */}
+        <Entity
+          id="mouseCursor"
+          cursor="rayOrigin: mouse"
+          mouse-cursor
+        ></Entity>{" "}
+        <a-cursor
+          id="mouseCursor"
+          animation__click="property: scale; startEvents: click; easing: easeInCubic; dur: 150; from: 0.2 0.2 0.2; to: 1 1 1"
+          cursor="fuse: true; fuseTimeout: 1"
+          material="color: #ffffff"
+        ></a-cursor>
+      </a-camera>
       {returnSquares}
-      {/* Mouse cursor */}
-      <Entity id="mouseCursor" cursor="rayOrigin: mouse" mouse-cursor></Entity>{" "}
+      {/* Walls, floor and ceiling */}
       <Entity
         geometry={{ primitive: "plane", width: 10, height: 10 }}
         position="0 0 -4"
@@ -96,6 +134,7 @@ const App = () => {
         rotation="90 0 0"
         material="color: #7BC8A4"
       ></Entity>
+      {/* Keyboard */}
       <Entity
         id="keyboard"
         super-keyboard={{ hand: "#mouseCursor" }}
@@ -109,6 +148,28 @@ const App = () => {
           },
         }}
       ></Entity>
+      <Entity
+        id="buttonStd"
+        ui-button={{
+          base: "beveled-square, blue",
+          top: "square, darkgreen",
+          pressed: "yellow, offset",
+        }}
+        position="0 0.4 -0.2"
+        rotation="30 0 0"
+        scale="3 3 3"
+        events={{
+          pressed: () => {
+            sendMessage();
+          },
+        }}
+      >
+        <Entity
+          text={{ value: "SEND" }}
+          rotation="-90 0 0"
+          position="0.445 0.04 0"
+        ></Entity>
+      </Entity>
     </a-scene>
   );
 };
